ee7aed96a318705457b0339fcaa1d87cca63de87
Luis Laguado <luisalfonso099@gmail.com>
2022-05-01 14:38:24 -0300

    escalera de clasificacion resuelto

--- EscaladaDeCalificacion.js
+++ EscaladaDeCalificacion.js
@@ -1,89 +1,44 @@
 
 const escalaDeClasificacion = (ranked, player)=>{
   const ranking = [...new Set(ranked)].reverse();
-  const posiciones = [];
-  const max = ranked[0];
-  const min = ranking[0];
+    const posiciones = [];
+    let long = ranking.length;
+    const max = ranked[0];
+    let x = 0;
+  player.forEach(el => {
+    
+      for (let j = x; j < long; j++) {
+        let pos = parseInt(ranking[j]);
+        // console.log("elemento",el);
 
-  let long = ranking.length;
-  
-  let index = 0;
-  player.forEach((el,i )=> {
-      while ( posiciones.length < player.length){
+        // console.log("posicion",pos);
 
-          const pos = ranking[index];
-          console.log(pos,el);
-
-            if(el < min){
-              posiciones.push(ranking.length +1)
-              index ++
-              break
-            }
-            if(el == pos){
-              console.log('elemento igua',el);
-              posiciones.push(long - index );
-              index ++
-             break;
-           }
-            if(el < pos){
-              posiciones.push(long - index +1);
-              index ++
-              break;
-            }
-            if(el > max){
-              posiciones.push(1)
-             index ++
-              break
-            }
-            console.log('bucle de' , pos, el);
-            break
-
-      }
+        if(el < pos){
+          // console.log('elemento es menor', el,pos);
+          posiciones.push(long - j +1);
+          x = j
+          break;
+        }
+        if(el == pos){
+          // console.log('iguales', el,pos);
+          posiciones.push(long - j);
+          break;
+        }
+        if(el > max){
+          // console.log('elemento es mayor al maximo', el,pos);
+          posiciones.push(1);
+          break;
+        }
+       
+    }
   })
-  console.log(ranking);
-  console.log(player);
-  console.log(posiciones);
+    // console.log(ranking);
+    // console.log(player);
+    // console.log(posiciones);
 
-  return posiciones
+    return posiciones
 
 };
 
-escalaDeClasificacion([100,100,50,40,40,20,10],[5,25,50,120]);
-
-
-
-//,const escalaDeClasificacion = (ranked, player)=>{
-//   const ranking = [...new Set(ranked)].reverse();
-//     const posiciones = [];
-//     let long = ranking.length;
-//     const max = ranked[0];
-//   player.forEach(el => {
-    
-//       for (let j = 0; j < long; j++) {
-//         let pos = parseInt(ranking[j]);
-//         console.log(pos);
-        
-//         if(el == pos){
-//           posiciones.push(long - j);
-//           break;
-//         }
-//         if(el > max){
-//           posiciones.push(1);
-//           break;
-//         }
-//         if(el < pos){
-//           posiciones.push(long - j +1);
-//           break;
-//         }
-//     }
-//   })
-//     console.log(ranking);
-//     console.log(player);
-//     console.log(posiciones);
-
-//     return posiciones
-
-// };
-
-// escalaDeClasificacion([100,90,90,80,75,60],[50,65,77,90,102]);
+escalaDeClasificacion([100,90,90,80,75,60],[50,65,77,90,102]);
 
